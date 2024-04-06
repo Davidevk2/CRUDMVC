3d6d8d2ed8014478ab642996e2c68687927e6427
Davidhzk2 <guffyherrera150@gmail.com>
2024-04-06 17:34:52 -0500

    Conexion y configuracion de appsettings

--- Controllers/SectorController.cs
+++ Controllers/SectorController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using Prueba.Data;
 
 namespace Prueba.Controllers
 {
    
    public class SectorsController : Controller{
+
+        public readonly SectorsContext _context;
+        
+        //Constructor que inicializa los modelos
+        public SectorsController(SectorsContext context){
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Prueba.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SectorsContext>(options =>
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("MySqlConnection"),
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql"))
+    );
+
 
 var app = builder.Build();
 
