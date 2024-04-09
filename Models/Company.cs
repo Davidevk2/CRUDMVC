8d53839d1713a06fa019f2c705c61d35bdad8c2e
Davidhzk2 <guffyherrera150@gmail.com>
2024-04-08 20:14:05 -0500

    Cambios en program

--- Models/Company.cs
+++ Models/Company.cs
@@ -2,7 +2,7 @@ namespace Prueba.Models
 {
     public class Company{
 
-        public int MyProperty { get; set; }
+        public int Id{ get; set; }
         public string Name { get; set; }
         public string Nit { get; set; }
         public string Phone { get; set; }
